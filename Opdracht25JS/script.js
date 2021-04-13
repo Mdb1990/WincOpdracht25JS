1a679d09497e903343c1b02a709e606e9282b4d4
Marco de boer <76598975+Mdb1990@users.noreply.github.com>
2021-04-13 21:09:27 +0200

    Add files via upload

---
+++ Opdracht25JS/script.js
@@ -0,0 +1,12 @@
+/** @format */
+
+const wallColor = function (colorWall1, colorWall2) {
+  console.log("The wall has been painted " + colorWall1);
+  console.log("The second wall has been painted " + colorWall2);
+};
+
+const Wall1 = "green";
+const Wall2 = "orange";
+
+wallColor(Wall1, Wall2);
+wallColor("magenta", "OliveGreen");
